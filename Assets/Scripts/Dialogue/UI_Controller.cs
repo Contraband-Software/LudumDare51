be4cd5a194e74ebff2aba8d841019a79cefd4fba
jakub bala <jakubbala04@gmail.com>
2022-10-01 17:19:38 +0100

    imported LeanTween

--- Assets/Scripts/Dialogue/UI_Controller.cs
+++ Assets/Scripts/Dialogue/UI_Controller.cs
@@ -12,6 +12,11 @@ public class UI_Controller : MonoBehaviour
     [SerializeField] TextMeshProUGUI speakerName_text;
     [SerializeField] List<TextMeshProUGUI> dialogueOptions_text = new List<TextMeshProUGUI>();
 
+
+    private List<string> current_DialogueOptions = new List<string>();
+    private int diagOptions = 0;
+    private string current_SpeakerName;
+
     private void Awake()
     {
 
@@ -22,6 +27,12 @@ public class UI_Controller : MonoBehaviour
     //shows UI for the given data. Should be CALLED ONCE per dialogue change
     public void DrawNode(string speakerName, List<string> dialogueOptions)
     {
+        InstantSoftClearDialogueOptions();
+
+        current_SpeakerName = speakerName;
+        current_DialogueOptions = dialogueOptions;
+        diagOptions = dialogueOptions.Count;
+
         //UPDATE TEXTS TO REFLECT RESPONSE PROMPT
         speakerName_text.text = speakerName + ": ";
         float smallestTextSize = Mathf.Infinity;
@@ -43,11 +54,67 @@ public class UI_Controller : MonoBehaviour
 
     }
 
-
+    //prompts a test dialogue
     private void OnDialogueTest()
     {
         print("P pressed");
 
         DrawNode("Jakub", new List<string> { "How are you?", "I hate you", "Im hungy" });
     }
+
+    #region DIALOGUE OPTION HANDLING (CLUNKY AS SHIT)
+    private void Dialogue1()
+    {
+        if(diagOptions >= 1)
+        {
+            SendDialogueChosen(1);
+        }
+    }
+    private void Dialogue2()
+    {
+        if (diagOptions >= 2)
+        {
+            SendDialogueChosen(2);
+        }
+    }
+    private void Dialogue3()
+    {
+        if (diagOptions >= 3)
+        {
+            SendDialogueChosen(3);
+        }
+    }
+    private void Dialogue4()
+    {
+        if (diagOptions >= 4)
+        {
+            SendDialogueChosen(4);
+        }
+    }
+    #endregion
+
+    private void SendDialogueChosen(int indexChosen)
+    {
+        print(indexChosen);
+    }
+
+
+
+
+    #region DIALOGUE VISIBILITY CONTROLS
+    //changes dialogue option texts to be blank
+    private void InstantSoftClearDialogueOptions()
+    {
+        foreach (TextMeshProUGUI t in dialogueOptions_text)
+        {
+            t.text = "";
+        }
+    }
+
+    //fades out entire dialogue panel
+    private void FadeOutDialoguePanel()
+    {
+
+    }
+    #endregion
 }
