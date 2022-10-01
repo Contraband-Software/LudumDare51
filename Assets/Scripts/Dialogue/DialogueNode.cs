9ed1568e66c14dfaa5d4ea723df1751963c0390a
CYANSERAPH <samtaseff@gmail.com>
2022-10-01 15:02:34 +0100

    Started graphdialodue system

---
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+using static GraphSystem.GraphConnections;
+
+namespace GraphSystem
+{
+    public class DialogueNode : Node
+    {
+        public struct DialogueData
+        {
+            public string name;
+            public string dialog;
+        }
+
+        [SerializeField, TextArea] private string name;
+        [SerializeField, TextArea(5, 100)] private string dialogue;
+
+        private DialogueData dialogueData;
+
+        [Input(dynamicPortList = true)]
+        public ResponseConnection previous;
+
+        [Output(dynamicPortList = true)]
+        public List<ResponseConnection> responses;
+
+        protected override void Init()
+        {
+            base.Init();
+
+            dialogueData = new DialogueData();
+            dialogueData.name = name;
+            dialogueData.dialog = dialogue;
+        }
+
+        public override object GetValue(NodePort port)
+        {
+            return dialogueData;
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogue/GraphConnections.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GraphSystem
+{
+    public class GraphConnections
+    {
+        [Serializable]
+        public struct ResponseConnection
+        {
+            public string response;
+        }
+    }
+}
