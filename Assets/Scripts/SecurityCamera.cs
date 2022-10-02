d07470b046825d3b7b8727c0304ce540b25eb26e
JuliusVanderArend <gamingpotatostudios@gmail.com>
2022-10-02 17:28:01 +0100

    added security camera

---
+++ Assets/Scripts/SecurityCamera.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCamera : MonoBehaviour
+{
+    public GameObject target;
+    public bool targetDetected = false;
+    public float detectionTresh;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = target.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraToTarget = Vector3.Normalize(target.transform.position - this.transform.position);
+        if(Vector3.Dot(cameraToTarget,this.transform.right)> detectionTresh)
+        {
+            RaycastHit hit;
+            if(!Physics.Raycast(transform.position,cameraToTarget,out hit) || hit.transform.gameObject==target)
+            {
+                targetDetected = true;
+            }
+            else
+            {
+                targetDetected = false;
+            }
+        }
+        else
+        {
+            targetDetected = false;
+        }
+        Debug.Log(targetDetected);
+    }
+}
