aebc08afb5151ec61c66e584293afd5402371d56
CYANSERAPH <samtaseff@gmail.com>
2022-10-02 13:57:48 +0100

    Fixed prototype

--- Assets/Scenes/SamPrototyping/MiniGameController.cs
+++ Assets/Scenes/SamPrototyping/MiniGameController.cs
@@ -1,14 +1,40 @@
+using GraphSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class MiniGameController : MonoBehaviour
+public class MiniGameController : MonoBehaviour//GraphSystem.AbstractTriggeredDialogue
 {
     private void Start()
     {
         GetComponent<GraphSystem.ActionComponent>().action = Meth;
     }
+
+    //public override void Suspend(bool status)
+    //{
+    //    throw new System.NotImplementedException();
+    //}
+
+    //public override NodeData Initiate()
+    //{
+    //    throw new System.NotImplementedException();
+    //}
+
+    //public override NodeData AdvanceDialogue(int responseIndex)
+    //{
+    //    throw new System.NotImplementedException();
+    //}
+
+    //public override NodeData GetCurrentDialogue()
+    //{
+    //    throw new System.NotImplementedException();
+    //}
+
+    //public override bool IsComplete()
+    //{
+    //    throw new System.NotImplementedException();
+    //}
 
 
 
